8ea038ab611725a2114ea6fd6babfdde30947815
Arthur Albuquerque Dos reis <127634288+DevArthReis@users.noreply.github.com>
2024-11-25 16:17:49 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MeuApp
+{
+    class Tipos
+    {
+        static void Main(string[] args)
+        { 
+            //Entendendo como funciona os tipos
+        const int IDADE_MINIMA = -18;
+        Console.WriteLine(IDADE_MINIMA);       
+        string nome = "Arthur";
+        Console.WriteLine(nome);
+        var texto = "testando";
+        Console.WriteLine(texto);
+        double idades = 2.3;
+        Console.WriteLine(idades);
+        float idade = 2.3f;
+        Console.WriteLine(idade);
+        bool usuarioJaCadastrado = false;
+        bool pagamentoRecebido = true;
+        var usuarioExpirado = false;
+        char letra = 'D';
+        string nomeclatura ="Arthur";
+        //Convertendo Tipos
+        int inteiro = 100;
+        float real = 25.5f;
+        //inteiro = (int)real;
+        //Console.WriteLine(inteiro);
+       // int valor1 = 100;
+       // float valor2 = 25.2f;
+        //valor2 = valor1;
+        //Console.WriteLine(valor2);//Valor vai ser 100.0f
+        //string valorReal = real.ToString();
+        //inteiro = int.Parse(valorReal);
+        inteiro = Convert.ToInt32(real);
+        Console.WriteLine(inteiro); 
+       // Console.WriteLine(valorReal); 
+        
+        }
+    }
+}
