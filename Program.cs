64f8b14488b48b5d9fccaf46cfaa13b61be459dc
arthur albuquerque <arthuralbuquerque.hsh@gmail.com>
2024-11-27 16:44:58 -0300

    Atualização de estudos

--- Program.cs
+++ Program.cs
@@ -36,7 +36,48 @@ namespace MeuApp
         inteiro = Convert.ToInt32(real);
         Console.WriteLine(inteiro); 
        // Console.WriteLine(valorReal); 
-        
+       //Operadores aritmetricos
+        var x = 5;
+        var y = 2.3;
+        var soma = x + y;
+        Console.WriteLine(soma);
+        var divisao = x / y;
+        Console.WriteLine(divisao);
+        var multiplicacao = x * y;
+        Console.WriteLine(multiplicacao);
+        var subtracao = x - y;
+        Console.WriteLine(subtracao);
+        var resto = x % y;
+        Console.WriteLine(resto);
+        //Operadores de comparação
+        var xy = 10;
+        Console.WriteLine(xy>11);
+        Console.WriteLine(xy>=11);
+        Console.WriteLine(xy<=11);
+        Console.WriteLine(xy<11);
+        Console.WriteLine(xy!=11);
+        Console.WriteLine(xy==10);
+        Console.WriteLine(xy==11);
+        //Operadores Logicos
+        //not(!) , or(||) , and(&&) são sempre uasdos em condicionais
+        int d = 12;
+        bool entre = (d>25)&& (d<40);
+        bool k = (d>10) && (d<20);
+        Console.WriteLine(entre);
+        Console.WriteLine(k);
+        bool ou = (d>25)|| (d<40);
+        Console.WriteLine(ou);
+        bool not = !(x<25);
+        Console.WriteLine(not);
+        //Condicionais
+        var a = 10;
+        var b =20;
+        if(b<a){
+            Console.WriteLine("Testetando uma condicional no c#");
+        }
+        else{
+            Console.WriteLine("Opa");
+        }
         }
     }
 }
