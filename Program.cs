5c31708c03b7cb93b9c3e72bd782c725200e866d
arthur albuquerque <arthuralbuquerque.hsh@gmail.com>
2024-11-30 15:22:46 -0300

    Atualização Struct e Enum

--- Program.cs
+++ Program.cs
@@ -6,17 +6,36 @@ namespace MeuApp
     {
         static void Main(string[] args)
         { 
-        
-        //Função
-        MeuMetodo();
-        string nome = RetornaNome("Arthur","Albuquerque");
-        Console.WriteLine(nome);
-        static void MeuMetodo(){
-            Console.WriteLine("C# é legal");
-        }
-        static string RetornaNome(string nome,string sobrenome){
-            return nome +" "+ sobrenome;
 
+            Product mouse = new Product(1,"MouseGamer",32.50,EproductType.Product);
+            var manutencaoEletrica = new Product(2,"manutenção eletrica residencial ",500,EproductType.Service);
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Name);
+            Console.WriteLine(mouse.Price);
+            Console.Write(mouse.Type);
+            Console.Write((int)mouse.Type);
         
+        
+        }
     }
-}}}
+    struct Product{
+        public Product(int id,string name,double price, EproductType type){
+            Id = id;
+            Name = name;
+            Price =  price;
+            Type = type;
+        }
+        public int Id;
+        public string Name;
+        public double Price;
+        public EproductType Type;
+        public double PriceInDolar(double dolar){
+            return Price * dolar;
+        }
+    }
+    enum EproductType{
+        Product = 1,
+        Service = 2
+
+    }
+}
